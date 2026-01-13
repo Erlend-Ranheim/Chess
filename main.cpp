4250899421425a05f15acf853e2ab648798af2e1
Erlend Ranheim <ranheim38@gmail.com>
2026-01-13 16:12:53 +0100

    Rendering works with a window, example piece placed and closing of window achieved

--- main.cpp
+++ main.cpp
@@ -5,21 +5,32 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
 
-    Game game;
+    std::array<const char*, 12> pieces = {
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/white-pawn.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/white-rook.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/white-knight.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/white-bishop.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/white-queen.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/white-king.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/black-pawn.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/black-rook.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/black-knight.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/black-bishop.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/black-queen.png",
+        "C:/Users/ranhe/CLionProjects/Chess/pieces-basic-png/black-king.png"
+    };
 
-
-    sf::Texture chessboardTexture;
-    if (!chessboardTexture.loadFromFile(
-        "C:/Users/ranhe/CLionProjects/Chess/Chess_Board.png"))
-    {
-        std::cerr << "Failed to load image\n";
-        return 1;
+    std::array<sf::Texture, 12> pieceTextures;
+    for (int i = 0; i < 12; ++i) {
+        if (!pieceTextures[i].loadFromFile(pieces[i])) {
+            throw std::runtime_error("failed to load texture from char");
+        }
     }
-
-    Renderer renderer(chessboardTexture);
+    Game game;
+    Renderer renderer(pieceTextures);
 
     while (renderer.isOpen()) {
-        renderer.render();
+        renderer.render(game);
     }
 
     return 0;
